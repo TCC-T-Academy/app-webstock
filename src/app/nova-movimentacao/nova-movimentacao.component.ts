6b93d873306fbb184930469d2b184a85b35ac2d1
Kelvin Evandro De Souza <f0fp1436@t-systems.com.br>
2022-11-24 20:38:40 -0300

    corrigido erro na entrada/saida de itens e adicionada pesquisa por id

--- src/app/nova-movimentacao/nova-movimentacao.component.ts
+++ src/app/nova-movimentacao/nova-movimentacao.component.ts
@@ -11,11 +11,14 @@ import { MovimentacaoService } from '../movimentacao.service';
 
 export class NovaMovimentacaoComponent implements OnInit{  
 
-  @Output("consultarTodas") consultarTodas: EventEmitter<any> = new EventEmitter();
+  @Output("consultar") consultar: EventEmitter<any> = new EventEmitter();
 
   nMov:INovaMovimentacao = {idItem:0,idUsuario:0,origemDestino:"",quantidade:0};
   inOut: string; 
   tipos: string[] = ["Entrada","Saida"]
+  orgDest: string = "Origem:"
+  disableCtrl: boolean = false
+  itemPesquisa: number = 0
   
 
   
@@ -27,23 +30,36 @@ export class NovaMovimentacaoComponent implements OnInit{
   
   enviar(mov:INovaMovimentacao){
     mov.idUsuario = 1
+
+    console.log(mov)
+
     if(this.inOut == this.tipos[0]){
       
       this.service.entradaItem(mov)
       .subscribe(data => {
-                    this.consultarTodas.emit();
+                    this.consultar.emit();
                     this.ngOnInit();
                     console.log(data)})
 
     }else if(this.inOut == this.tipos[1]){
       
       this.service.saidaItem(mov).subscribe(data => {
-        this.consultarTodas.emit();
-        this.ngOnInit();
-        console.log(data)
+          this.consultar.emit();
+          this.ngOnInit();
+          console.log(data)
       })
     }
 
+  }
+
+
+  verSelecao(sel:string){
+    this.orgDest = (sel == this.tipos[1])? "Origem:" : "Destino:";
+    //this.disableCtrl = (sel == this.tipos[2])? true : false;
+  }
+
+  buscar(item:number){
+    this.consultar.emit(item)
   }
 
 }
