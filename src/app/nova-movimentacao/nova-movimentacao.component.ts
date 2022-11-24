32dc113f7152b8fc0ded15592ceb114e1bc4553b
Kelvin Evandro De Souza <f0fp1436@t-systems.com.br>
2022-11-24 18:22:11 -0300

    atualizacao da lista de movimentacoes ao se inserir uma nova

--- src/app/nova-movimentacao/nova-movimentacao.component.ts
+++ src/app/nova-movimentacao/nova-movimentacao.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { INovaMovimentacao } from 'src/interfaces/interface';
 import { MovimentacaoService } from '../movimentacao.service';
 
@@ -9,24 +9,39 @@ import { MovimentacaoService } from '../movimentacao.service';
   styleUrls: ['./nova-movimentacao.component.css']
 })
 
-export class NovaMovimentacaoComponent {
+export class NovaMovimentacaoComponent implements OnInit{  
+
+  @Output("consultarTodas") consultarTodas: EventEmitter<any> = new EventEmitter();
 
   nMov:INovaMovimentacao = {idItem:0,idUsuario:0,origemDestino:"",quantidade:0};
-  inOut: string=""; 
+  inOut: string; 
   tipos: string[] = ["Entrada","Saida"]
   
-  constructor(private service:MovimentacaoService){
 
+  
+  constructor(private service:MovimentacaoService){
+    this.inOut = ""
   }
+
+  ngOnInit(): void {}
   
   enviar(mov:INovaMovimentacao){
     mov.idUsuario = 1
-    console.log(mov)
+    if(this.inOut == this.tipos[0]){
+      
+      this.service.entradaItem(mov)
+      .subscribe(data => {
+                    this.consultarTodas.emit();
+                    this.ngOnInit();
+                    console.log(data)})
 
-    if(this.inOut = this.tipos[0]){
-      this.service.entradaItem(mov).subscribe(data => console.log(data))
-    }else if(this.inOut = this.tipos[1]){
-      this.service.saidaItem(mov).subscribe(data => console.log(data))
+    }else if(this.inOut == this.tipos[1]){
+      
+      this.service.saidaItem(mov).subscribe(data => {
+        this.consultarTodas.emit();
+        this.ngOnInit();
+        console.log(data)
+      })
     }
 
   }
