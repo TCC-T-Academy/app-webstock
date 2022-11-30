d10f855ca67a6138aaaf4a853a796c6279da901a
Kelvin Evandro De Souza <f0fp1436@t-systems.com.br>
2022-11-30 08:42:12 -0300

    adicionada rota para teste de paginacao

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EstoqueComponent } from './estoque/estoque.component';
 import { MovimentacaoComponent } from './movimentacao/movimentacao.component';
+import { PrevisaoTestComponent } from './previsao-test/previsao-test.component';
 import { PrevisaoComponent } from './previsao/previsao.component';
 import { ReservaComponent } from './reserva/reserva.component';
 
@@ -21,6 +22,10 @@ const routes: Routes = [
   {
     path:'previsoes',
     component: PrevisaoComponent
+  },
+  {
+    path:'previsoes2',
+    component: PrevisaoTestComponent
   }
   
 ];
