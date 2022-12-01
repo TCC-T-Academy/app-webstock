9432dc488d1f05e06a0443f6259715dd4d2cd179
Kelvin Evandro De Souza <f0fp1436@t-systems.com.br>
2022-12-01 16:10:32 -0300

    Alteracao para esconder side-nave.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 import { BreakpointObserver} from '@angular/cdk/layout';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
 
 
 @Component({
@@ -11,7 +11,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent{
   title = 'app-webstock';
   
 shouldRun: any;
@@ -19,21 +19,39 @@ public sidebarShow: boolean = true;
 
 @ViewChild(MatSidenav)
 sidenav!:MatSidenav;
+isLogin:Boolean = true;
 
 constructor(private observer: BreakpointObserver, private router: Router){
-
-}
- ngAfterViewInit(){
-  this.observer.observe(['(max-width: 800px)']).subscribe((res) =>{
-    if(res.matches){
-      this.sidenav.mode = 'over';
-      this.sidenav.close();
-    }else{
-      this.sidenav.mode = 'side';
-      this.sidenav.open();
+  router.events.subscribe((res) => {
+    console.log(res instanceof NavigationEnd);
+    if(res instanceof NavigationEnd){
+      if(this.router.url == "/login"){
+        this.isLogin=true;
+     }else{
+        this.isLogin=false;
+     }
     }
-  });
- }
+  })
+}
+
+ngAfterViewInit(){
+
+    this.observer.observe(['(max-width: 800px)']).subscribe((res) =>{
+      if(!this.isLogin){
+        if(res.matches){        
+          this.sidenav.mode = 'over';
+          this.sidenav.close();
+        }else{
+          this.sidenav.mode = 'side';
+          this.sidenav.open();
+        }
+      } else {
+        this.sidenav.close();
+      }
+
+      
+    });
+}
 
  isLoginRoute() {
     return this.router.url === '/login';
