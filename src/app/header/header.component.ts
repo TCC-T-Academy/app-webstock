9fa6ddcf29ba2514e380b996b504e071d8dbb280
Kelvin Evandro De Souza <f0fp1436@t-systems.com.br>
2022-12-07 08:27:54 -0300

    criado componente header com a toolbar

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,44 @@
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component, Input, Output } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  @Input("headerName") headerName: string = ""
+
+  title = 'app-webstock';
+  shouldRun: any;  
+  isLogin:Boolean = false;
+  
+
+  
+  constructor(private router: Router){
+  
+    router.events.subscribe((res) => {
+      if(res instanceof NavigationEnd){
+        if(this.router.url == "/login"){
+          this.isLogin=true;
+       } else{
+          this.headerName = this.router.url
+          console.log(this.router);
+          this.isLogin=false;
+       }
+      }
+    })
+  }
+  
+  deslogar(){
+  
+      if(localStorage.getItem('token')!= ""){
+  
+        localStorage.removeItem('token');
+  
+      }
+  
+   }
+
+}
