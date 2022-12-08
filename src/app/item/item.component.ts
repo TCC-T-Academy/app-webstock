100bbf9cbca927ecba9fdbe45b2291095af45d97
Kelvin Evandro De Souza <f0fp1436@t-systems.com.br>
2022-12-08 16:13:28 -0300

    alterada exibicao para tabela

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from '@angular/core';
-import { IItem } from 'src/interfaces/interface';
+import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
+import { Subject } from 'rxjs';
+import { IEstoque, IItem } from 'src/interfaces/interface';
 import { ItemService } from '../item.service';
 
 @Component({
@@ -7,12 +9,28 @@ import { ItemService } from '../item.service';
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
-export class ItemComponent {
+export class ItemComponent implements OnInit{
+  @Input() changing: Subject<IEstoque> = new Subject();
+  @Input() item: IItem = {
+    descricao:"teste",
+    familia:"teste",
+    estoqueSeguranca:0,
+    grupo:"",
+    unidade:"",
+    idItem:0
+  };
 
-  @Input() item: any;
+  dataSource:MatTableDataSource<IItem>;
   
+  displayedColumns:string[] = ['id','descricao','familia', 'grupo','unidade']
+
   constructor(){
+    this.dataSource = new MatTableDataSource();
+    this.dataSource.data.push(this.item);
   }
 
-  
+  ngOnInit(): void {
+    
+  }
+
 }
